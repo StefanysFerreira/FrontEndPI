26f7389b2d81402df3c4d188ee761d4bfa6e4769
HenriqueSantosHS <henriquelp321@gmail.com>
2023-05-01 17:45:47 -0300

    passando novos parametros

--- our_movies/src/paginas/Home.jsx
+++ our_movies/src/paginas/Home.jsx
@@ -1,34 +1,35 @@
 import { useState, useEffect } from "react";
+import MovieCard from "./components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-const Home =() => {
-    const [topMovies, setTopMovies] = useState([])
-
+const Home = () => {
+    const [topMovies, setTopMovies] = useState([]);
+  
     const getTopRatedMovies = async (url) => {
-
-        const res = await fetch(url)
-        const data = await res.json()
-
-        setTopMovies(data.results)
+      const res = await fetch(url);
+      const data = await res.json();
+      setTopMovies(data.results);
     };
-
+  
     useEffect(() => {
-        const topRatedUrl = `${moviesURL}top_rated${apiKey}`;
-
-        getTopRatedMovies(topRatedUrl);
+      const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+      console.log(topRatedUrl);
+      getTopRatedMovies(topRatedUrl);
     }, []);
-
+  
+    console.log(topMovies);
+  
     return (
-            <div className="container">
-              <h2 className="title">Melhores Filmes:</h2>
-              <div className="movies-container">
-                {topMovies.length === 0 && <p>Carregando...</p>}
-                {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
-              </div>
-            </div>
+      <div className="container">
+        <h2 className="title">Melhores Escolhas:</h2>
+        <div className="movies-container">
+          {topMovies.length > 0 &&
+            topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+      </div>
     );
-};
-
-export default Home;
+  };
+  
+  export default Home;
