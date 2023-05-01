154fda9024c3ca76dbf3db02cbb48bf9e48ff719
HenriqueSantosHS <henriquelp321@gmail.com>
2023-05-01 15:02:15 -0300

    adicionando parametros e o import Navbar

--- our_movies/src/App.jsx
+++ our_movies/src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> 
       <h2>Our Movies</h2>
+      <Outlet />
     </div>
   )
 }
