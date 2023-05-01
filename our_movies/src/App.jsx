f8e5f76912579d179253da6a841784fabc26f798
HenriqueSantosHS <henriquelp321@gmail.com>
2023-05-01 18:16:23 -0300

    atualização

--- our_movies/src/App.jsx
+++ our_movies/src/App.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> 
+    <div className="app-container">
+      <Navbar />
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
