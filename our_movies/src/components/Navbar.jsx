2c1edef934cb73f1a924998f6e7a1908a96da019
HenriqueSantosHS <henriquelp321@gmail.com>
2023-05-01 15:10:17 -0300

    Criando formulario

--- our_movies/src/components/Navbar.jsx
+++ our_movies/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
     return (
         <nav id="navbar">
             <h2>
-                <Link to="/">Our Movies</Link>
+                <Link to="/"><BiCameraMovie />Our Movies</Link>
             </h2>
+            <form>
+                <input type="text" placeholder="Que filme procura ?" />
+                <button type="submit"></button>
+                <BiSearchAlt2 />
+            </form>
         </nav>
     );
 };
